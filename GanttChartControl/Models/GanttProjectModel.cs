1cce67ade11ae948ffe44df1626343e9d3953743
代鑫 <daixin1031017817@163.com>
2020-01-15 20:58:03 +0800

    变更

---
+++ GanttChartControl/Models/GanttProjectModel.cs
@@ -0,0 +1,56 @@
+﻿using CommonLib;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanttChartControl.Models
+{
+    public class GanttProjectModel: NotifyPropertyChanged
+    {
+		private string _projectName;
+
+		/// <summary>
+		/// Get or set ProjectName value
+		/// </summary>
+		public string ProjectName
+		{
+			get { return _projectName; }
+			set { Set(ref _projectName, value); }
+		}
+		private DateTime _startTime;
+
+		/// <summary>
+		/// Get or set StartTime value
+		/// </summary>
+		public DateTime StartTime
+		{
+			get { return _startTime; }
+			set { Set(ref _startTime, value); }
+		}
+		private DateTime _endTime;
+
+		/// <summary>
+		/// Get or set EndTime value
+		/// </summary>
+		public DateTime EndTime
+		{
+			get { return _endTime; }
+			set { Set(ref _endTime, value); }
+		}
+
+		private ObservableCollection<GanttProjectModel> _children;
+
+		/// <summary>
+		/// Get or set Children value
+		/// </summary>
+		public ObservableCollection<GanttProjectModel> Children
+		{
+			get { return _children; }
+			set { Set(ref _children, value); }
+		}
+
+	}
+}
---
+++ GanttChartControl/ProjectItemTreeControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GanttChartControl
+{
+    public class ProjectTreeControl:TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            ProjectTreeItemControl item = new ProjectTreeItemControl();
+            return item;
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            bool _isTreeLVI = item is ProjectTreeItemControl;
+            return _isTreeLVI;
+        }
+
+    }
+    public class ProjectTreeItemControl : TreeViewItem
+    {
+        private int _level = -1;
+        public int Level
+        {
+            get
+            {
+                if (_level == -1)
+                {
+                    ProjectTreeItemControl parent =
+                        ItemsControl.ItemsControlFromItemContainer(this) as ProjectTreeItemControl;
+                    _level = (parent != null) ? parent.Level + 1 : 0;
+                }
+                return _level;
+            }
+        }
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            ProjectTreeItemControl item = new ProjectTreeItemControl();
+            return item;
+        }
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            bool _isITV = item is ProjectTreeItemControl;
+            return _isITV;
+        }
+    }
+}
